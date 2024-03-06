a71644d709a82ff97834a4d25066b36f0047e953
71Arid <no232el@gmail.com>
2024-03-06 09:37:03 +0300

    12 fixed

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,3 +1,10 @@
 export default function createReportObject(employeesList) {
-
+  const obj = {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments(all) {
+      const number = Object.keys(all).length;
+      return number;
+    },
+  };
+  return obj;
 }
