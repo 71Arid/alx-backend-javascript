16c483b0d9cdfbfc9c210d76a4be83232090e925
71Arid <no232el@gmail.com>
2024-03-06 11:56:23 +0300

    iterate through iterable object 101

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,9 @@
+export default function iterateThroughObject(reportWithIterator) {
+  let str = '';
+  for (const value of reportWithIterator) {
+    const addedStr = value ? `${value} | ` : '';
+    str += addedStr;
+  }
+  str = str.slice(0, -2);
+  return str;
+}
