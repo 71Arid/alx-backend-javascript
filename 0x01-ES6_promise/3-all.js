ab97c909434f5f2958259285654e9a67e97e2937
71Arid <no232el@gmail.com>
2024-03-07 14:02:29 +0300

    3-all.js method chainning

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,1 +1,15 @@
-import {} from 
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  let body;
+  let fname;
+  let sname;
+  uploadPhoto().then((value) => {
+    body = value.body;
+    return createUser();
+  }).then((value) => {
+    fname = value.firstName;
+    sname = value.lastName;
+  }).catch(() => console.log('Signup system offline'));
+  console.log(`${body} ${fname} ${sname}`);
+}
