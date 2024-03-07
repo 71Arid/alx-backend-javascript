8a838ad8458b824c719ededd205f1fa65588dfdc
71Arid <no232el@gmail.com>
2024-03-07 20:06:59 +0300

    fixed eslint error in 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,12 +5,8 @@ export default function handleProfileSignup(firstName, lastName, filename) {
   const user = signUpUser(firstName, lastName);
   const photo = uploadPhoto(filename);
   return Promise.allSettled([user, photo])
-    .then((vs) => {
-      return vs.map((v) => {
-        return {
-	  status: v.status,
-          value: v.status === 'fulfilled' ? v.value : String(v.reason),
-	};
-      });
-    });
+    .then((vs) => vs.map((v) => ({
+      status: v.status,
+      value: v.status === 'fulfilled' ? v.value : String(v.reason),
+    })));
 }
