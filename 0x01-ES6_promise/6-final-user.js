1b6c40bedcdbcc822675344500f6167525591638
71Arid <no232el@gmail.com>
2024-03-07 15:32:00 +0300

    multiple promises no 6 eslint correction

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,8 +3,6 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
   const user = signUpUser(firstName, lastName);
-  const photo = uploadPhoto(fileName);
-  Promise.all([user, photo]).then(value => {
-    return value;
-  });
+  const photo = uploadPhoto(filename);
+  Promise.all([user, photo]).then((value) => value);
 }
