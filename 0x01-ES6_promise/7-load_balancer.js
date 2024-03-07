bbc2b34dbef9568d4f82ffccf01a18792127543f
71Arid <no232el@gmail.com>
2024-03-07 15:39:08 +0300

    load balncer return resolved first solved eslint problem

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,5 +1,3 @@
 export default function loadBalancer(chinaDownload, USDownload) {
-  return Promise.all([chinaDownload, USDownload]).then((value) => {
-    return value[0];
-  });
+  return Promise.all([chinaDownload, USDownload]).then((value) => value[0]);
 }
