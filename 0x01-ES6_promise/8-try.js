0799ec5aba782d9679081b69726662374f5bceb2
71Arid <no232el@gmail.com>
2024-03-07 20:48:13 +0300

    removed catch block

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,12 +1,7 @@
 export default function divideFunction(numerator, denominator) {
-  try {
-    if (denominator === 0) {
-      throw Error('cannot divide by 0');
-    } else {
-      return (numerator / denominator);
-    }
-  } catch (error) {
-    console.log(error);
-    return undefined;
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
+  } else {
+    return (numerator / denominator);
   }
 }
