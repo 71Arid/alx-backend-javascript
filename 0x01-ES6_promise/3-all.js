fc572a32fb470956249c74a81b2eb701e20f44d2
71Arid <no232el@gmail.com>
2024-03-07 16:14:33 +0300

    returned the promise in the function in no 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const photo = uploadPhoto();
-  const user = createUser();
-  Promise.all([photo, user]).then((value) => {
+  return Promise.all([uploadPhoto(), createUser()]).then((value) => {
     console.log(value[0].body, value[1].firstName, value[1].lastName);
-    return value;
   }).catch(() => console.log('Signup system offline'));
 }
