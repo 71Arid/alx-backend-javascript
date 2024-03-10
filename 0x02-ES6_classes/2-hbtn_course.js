a575b9c2a28b997d01fd3f2eb72e76d1591cac69
71Arid <no232el@gmail.com>
2024-03-10 09:32:28 +0300

    hbtn course

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,37 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    }
+  }
+
+  set length(newLength) {
+    if (typeof newLength === 'number') {
+      this._length = newLength;
+    }
+  }
+
+  set students(newStudents) {
+    if (typeof newStudents === 'object') {
+      this._students = newStudents;
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+}
