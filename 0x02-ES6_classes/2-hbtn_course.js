1df950598d6742521c99a6fcf1525ef5411b8d07
71Arid <no232el@gmail.com>
2024-03-10 09:58:05 +0300

    added many type errors

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -6,25 +6,27 @@ export default class HolbertonCourse {
   }
 
   set name(newName) {
-    if (typeof newName === 'string') {
-      this._name = newName;
-    } else {
+    if (!(typeof newName === 'string')) {
       throw new TypeError('Name must be a string');
     }
+    this._name = newName;
   }
 
   set length(newLength) {
-    if (typeof newLength === 'number') {
-      this._length = newLength;
-    } else {
+    if (!(typeof newLength === 'number')) {
       throw new TypeError('Length must be a number');
     }
+    this._length = newLength;
   }
 
   set students(newStudents) {
-    if (typeof newStudents === 'object') {
-      this._students = newStudents;
+    if (!(newStudents instanceof Array)) {
+      throw new TypeError('Students must be an array of strings');
     }
+    if (!newStudents.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = newStudents;
   }
 
   get name() {
