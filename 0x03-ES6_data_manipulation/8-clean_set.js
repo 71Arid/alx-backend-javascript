61b7033170ecbd7d69a4f567c33797c19c16ba47
71Arid <no232el@gmail.com>
2024-03-13 00:05:03 +0300

    clesn set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+export default function cleanSet(set, startString) {
+  let str = '';
+  if (startString !== '') {
+    set.forEach((v) => {
+      const len = startString.length;
+      const sliced = v.slice(0, len);
+      if (sliced === startString) {
+        str += `${v.slice(len)}-`;
+      }
+    });
+    return str.slice(0, -1);
+  }
+  return str;
+}
