76011af784e4a41f0729c5e8e14e2658f474fa59
71Arid <no232el@gmail.com>
2024-03-12 21:13:13 +0300

    sum reduce added check

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,7 +1,7 @@
 export default function getStudentIdsSum(array) {
   let newarr = [];
   if (array instanceof Array) {
-    newarr = array.reduce((total, value) => value + total, 0);
+    newarr = array.reduce((total, value) => value.id + total, 0);
   } else {
     return 0;
   }
