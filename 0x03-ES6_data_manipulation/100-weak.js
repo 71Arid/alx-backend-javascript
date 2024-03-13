21b35facd924e9896f0f03e58cb865d52527dd3d
71Arid <no232el@gmail.com>
2024-03-13 12:11:18 +0300

    weakmap solving

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,11 +1,12 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endp) {
-  if (weakMap.has(endp) && (weakMap.get(endp) < 5)) {
-    weakMap.set(endp, weakMap.get(endp) + 1);
-  } else if (weakMap.get(endp) >= 5) {
+  if (!weakMap.has(endp)) {
+    weakMap.set(endp, 0);
+  }
+  if (weakMap.get(endp) === 4) {
     throw new Error('Endpoint load is high');
   } else {
-    weakMap.set(endp, 1);
+    weakMap.set(endp, weakMap.get(endp) + 1);
   }
 }
