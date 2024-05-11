0a63e4f538d09a50f29f7f9898406fa3600ba0c1
71Arid <no232el@gmail.com>
2024-05-11 20:09:42 +0300

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,11 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.once('data', (input) => {
+process.stdin.on('data', (input) => {
   const name = input.toString().trim();
   process.stdout.write(`Your name is: ${name}\n`);
-  process.stdout.write('This important software is now closing\n');
   process.exit(0);
 });
 
-module.exports = process;
+process.stdin.on('end', () => {
+  process.stdout('This important software is now closing\n');
+});
