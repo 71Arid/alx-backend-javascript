d3aafa4ee5a203796fa0f0b336f9b2f8ce813a8b
71Arid <no232el@gmail.com>
2024-05-12 15:13:01 +0300

    read_file

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8').split('\n').filter(line => line.trim() !== '');
+    const students = {};
+        
+    data.forEach(row => {
+      const fields = row.split(',');
+      const field = fields[fields.length - 1];
+      if (students[field]) {
+        students[field].push(fields[0]);
+      } else {
+        students[field] = [fields[0]];
+      }
+    });
+
+    console.log(`Number of students: ${data.length}`);
+          
+    for (const field in students) {
+        console.log(`Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`);
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
