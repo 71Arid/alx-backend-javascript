28c81d179a6c1786e4621171f399b68630a70fb6
71Arid <no232el@gmail.com>
2024-05-13 14:06:33 +0300

    http cmplex server

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -2,35 +2,35 @@ const http = require('http');
 const fs = require('fs').promises;
 
 async function countStudents(path) {
-    try {
-      const filedata = await fs.readFile(path, 'utf-8');
-      const lines = filedata.split('\n');
-      let response = '';
-      let data = lines.filter((line) => line.trim() !== '');
-      data = data.slice(1);
-      const students = {};
-      data.forEach((row) => {
-        const fields = row.split(',');
-        const field = fields[fields.length - 1];
-        if (students[field]) {
-          students[field].push(fields[0]);
-        } else {
-          students[field] = [fields[0]];
-        }
-      });
-  
-      response += `Number of students: ${data.length}\n`;
-  
-      for (const field in students) {
-        if (Object.prototype.hasOwnProperty.call(students, field)) {
-          response += `Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}\n`;
-        }
+  try {
+    const filedata = await fs.readFile(path, 'utf-8');
+    const lines = filedata.split('\n');
+    let response = '';
+    let data = lines.filter((line) => line.trim() !== '');
+    data = data.slice(1);
+    const students = {};
+    data.forEach((row) => {
+      const fields = row.split(',');
+      const field = fields[fields.length - 1];
+      if (students[field]) {
+        students[field].push(fields[0]);
+      } else {
+        students[field] = [fields[0]];
       }
-      return response.slice(0,-1);
-    } catch (error) {
-      throw new Error('Cannot load the database');
+    });
+  
+    response += `Number of students: ${data.length}\n`;
+  
+    for (const field in students) {
+      if (Object.prototype.hasOwnProperty.call(students, field)) {
+        response += `Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}\n`;
+      }
     }
+    return response.slice(0,-1);
+  } catch (error) {
+    throw new Error('Cannot load the database');
   }
+}
 
 const hostname = 'localhost';
 const port = 1245;
