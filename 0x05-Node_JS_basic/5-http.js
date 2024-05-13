4d17050d667492ce8e2f4091d6cd3a6f07f9ebe2
71Arid <no232el@gmail.com>
2024-05-13 14:01:05 +0300

    http cmplex server

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,61 @@
+const http = require('http');
+const fs = require('fs').promises;
+
+async function countStudents(path) {
+    try {
+      const filedata = await fs.readFile(path, 'utf-8');
+      const lines = filedata.split('\n');
+      let response = '';
+      let data = lines.filter((line) => line.trim() !== '');
+      data = data.slice(1);
+      const students = {};
+      data.forEach((row) => {
+        const fields = row.split(',');
+        const field = fields[fields.length - 1];
+        if (students[field]) {
+          students[field].push(fields[0]);
+        } else {
+          students[field] = [fields[0]];
+        }
+      });
+  
+      response += `Number of students: ${data.length}\n`;
+  
+      for (const field in students) {
+        if (Object.prototype.hasOwnProperty.call(students, field)) {
+          response += `Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}\n`;
+        }
+      }
+      return response.slice(0,-1);
+    } catch (error) {
+      throw new Error('Cannot load the database');
+    }
+  }
+
+const hostname = 'localhost';
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+  } else if (url === '/students') {
+    countStudents(process.argv[2])
+      .then((content) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(`This is the list of our students\n${content}`);
+      })
+      .catch((error) => {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(`Internal Server Error\n${error}`);
+      });
+  }
+});
+
+app.listen(port, hostname);
+
+module.exports = app;
