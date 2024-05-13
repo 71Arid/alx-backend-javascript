0aa98212113f84876a3ca053cde3c30ae51236fe
71Arid <no232el@gmail.com>
2024-05-13 12:42:59 +0300

    read file asynchronously

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,30 @@
+const fs = require('fs').promises;
+
+async function countStudents(path) {
+  try {
+    let data = await fs.readFile(path, 'utf-8').split('\n').filter((line) => line.trim() !== '');
+    data = data.slice(1);
+    const students = {};
+    data.forEach((row) => {
+      const fields = row.split(',');
+      const field = fields[fields.length - 1];
+      if (students[field]) {
+        students[field].push(fields[0]);
+      } else {
+        students[field] = [fields[0]];
+      }
+    });
+
+    console.log(`Number of students: ${data.length}`);
+
+    for (const field in students) {
+      if (Object.prototype.hasOwnProperty.call(students, field)) {
+        console.log(`Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`);
+      }
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents
