394ea5d319a385dda6e377d8f7f9b5d36fadf5fc
71Arid <no232el@gmail.com>
2024-05-13 12:25:45 +0300

    readfile

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -18,7 +18,9 @@ function countStudents(path) {
     console.log(`Number of students: ${data.length}`);
 
     for (const field in students) {
-      console.log(`Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`);
+      if (Object.prototype.hasOwnProperty.call(students, field)) {
+        console.log(`Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`);
+      }
     }
   } catch (error) {
     throw new Error('Cannot load the database');
