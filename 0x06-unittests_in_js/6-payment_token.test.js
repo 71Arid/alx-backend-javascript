cc5783fe67146d8b61fa696242c4df18797634ad
71Arid <no232el@gmail.com>
2024-05-15 15:45:29 +0300

    handling promises with done

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,16 @@
+const chai = require('chai');
+const expect = chai.expect;
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', function() {
+  it('should return response if success', function(done) {
+    getPaymentTokenFromAPI(true)
+      .then((response) => {
+        expect(response).to.deep.equal({ data: 'Successful response from the API' });
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+});
